34a6f6370dc6f06572ddb3e4179fc004d3962822
Matheus Camba <mhscamba@gmail.com>
2022-03-14 18:04:54 -0300

    rotas att

--- src/routes/fornecedorRoute.js
+++ src/routes/fornecedorRoute.js
@@ -3,14 +3,18 @@ import FornecedorController from '../controller/FornecedorController.js'
 
 const router = Router();
 
-router.post('/fornecedor/criartabela', FornecedorController.criarTabela)
-
 router.get('/fornecedor', (req, res)=>{
-    res.send('Rota GET funciona')
+    res.send('Rota GET funcionando.')
 })
 
+router.post('/fornecedor/criartabela', FornecedorController.criarTabela)
+
+router.get('/fornecedor/buscartodosfornecedores', FornecedorController.getTodosFornecedores)
+
+router.post('/fornecedor/novofornecedor', FornecedorController.postarNovoFornecedor)
+
 router.post('/fornecedor', (req, res)=> {
-    res.status(200).json(`Rota funciona`)
+    res.status(200).json(`Rota funcionando`)
 })
 
 export default router
