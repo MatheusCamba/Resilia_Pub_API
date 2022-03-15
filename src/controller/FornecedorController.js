8baf00d601be9593b0ee070efa829a5bf6724886
Matheus Camba <mhscamba@gmail.com>
2022-03-15 16:27:21 -0300

    try/catch add

--- src/controller/FornecedorController.js
+++ src/controller/FornecedorController.js
@@ -11,11 +11,14 @@ class FornecedorController{
             telefone NUMBER
         )`
         bdFornecedor.run(tabela_fornecedor, (e) => {
-            if(e){
-                res.status(404).send('Erro ao criar tabela', e.message)
-            } else {
-                res.status(200).send('Tabela criada com sucesso.')
+            try{
+                if(!e){
+                    res.status(200).send('Tabela criada com sucesso.')
+                } 
+            }catch(error) {
+                res.status(404).send('Erro ao criar a tabela: ', error.message)
             }
+            
         })
     }
     //METODO DE BUSCA DE TODOS
@@ -24,13 +27,11 @@ class FornecedorController{
         SELECT * FROM fornecedor`
         bdFornecedor.all(busca_todosFornecedores, (e, result) =>{
             try{
-                if(e){
-                    res.status(404).send('Error ao buscar', e.message)
-                } else {
+                if(!e){
                     res.status(200).json(result)
                 }
             } catch(error){
-                    res.status(404).send('Error ao buscar', error.message)
+                res.status(404).send('Error ao buscar', error.message)
             }
         })
     }
@@ -49,14 +50,11 @@ class FornecedorController{
         INSERT INTO fornecedor (nome, cnpj, telefone) VALUES ('${newFornecedor.nome}', ${newFornecedor.cnpj}, ${newFornecedor.telefone})`
         bdFornecedor.run(novo_fornecedor, (e) =>{
             try{
-                if(e){
-                    res.status(404).send('ERRO')
-                }else{
+                if(!e){
                     res.status(200).send('Novo fornecedor incluído com sucesso!')
                 }
-                
             }catch(error){
-                res.status(404).send('Error ao buscar', error.message)
+                res.status(404).send('Error ao inserir o novo fornecedor: ', error.message)
             }
         })
     }
@@ -68,9 +66,7 @@ class FornecedorController{
 
         bdFornecedor.get(query_fornecedor, (e, row) =>{
             try{
-                if(e){
-                    res.status(404).send('Erro ao buscar o registro', e.message)
-                } else{
+                if(!e){
                     res.status(200).send(row)
                 }
             } catch(error){
