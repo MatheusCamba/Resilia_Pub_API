b18daa97b26dda153af80fb27af815c450d8f4cf
Matheus Camba <mhscamba@gmail.com>
2022-03-22 14:52:08 -0300

    att porta para deploy

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import routes from './routes/index.js';
 const app = express()
 app.use(express.json())
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=> {
     console.log(`Servidor rodando na porta ${port}`)
